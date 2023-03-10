f1d851108efdabd51551eb19d526fc95d001cb97
ngugimuchangi <tech.curious14@gmail.com>
2023-03-10 10:27:08 +0300

    fixed style

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,5 +1,5 @@
 export default function appendToEachArrayValue(array, appendString) {
-  const arrayCopy = [...array];
+  const arrayCopy = array;
   for (const value of arrayCopy) {
     arrayCopy[arrayCopy.indexOf(value)] = appendString + value;
   }
