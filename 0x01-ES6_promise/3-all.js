0fecd7274beb8822b10f4f95089e92b8aaf4f1f3
ngugimuchangi <tech.curious14@gmail.com>
2023-03-12 08:20:23 +0300

    promise concurrency

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,9 +1,11 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  uploadPhoto().then((response) => {
-    createUser().then((newUser) => {
-      console.log(`${response.body} ${newUser.firstName} ${newUser.lastName}`);
-    });
+  const userDetails = Promise.all([uploadPhoto(), createUser()]);
+  userDetails.then((results) => {
+    const { body } = results[0];
+    const { firstName } = results[1];
+    const { lastName } = results[1];
+    console.log(`${body} ${firstName} ${lastName}`);
   });
 }
