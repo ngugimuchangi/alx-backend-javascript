b2befc86dfad3eec968496d19dff502b9ecfcb38
ngugimuchangi <tech.curious14@gmail.com>
2023-03-12 09:19:49 +0300

    fix due to ambigous instructions

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,10 +1,10 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  const userDetails = Promise.all([uploadPhoto(), createUser()]);
-  userDetails
-    .then(([photo, user]) => {
-      console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
-    })
-    .catch((error = 'Signup system offline') => console.error(error));
+  return Promise.all([uploadPhoto(), createUser()]);
 }
+handleProfileSignup()
+  .then(([photo, user]) => {
+    console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
+  })
+  .catch((error = 'Signup system offline') => console.error(error));
