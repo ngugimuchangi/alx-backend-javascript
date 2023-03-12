69c7cd91ef3f68ef8ed405bf47e4ef8e93dd8ff5
ngugimuchangi <tech.curious14@gmail.com>
2023-03-12 09:03:33 +0300

    return a promise

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,6 +1,6 @@
 export default function handleResponseFromAPI(promise) {
-  promise
-    .then(() => ({ status: 200, body: 'success' }))
-    .catch((error) => console.error(error))
-    .finally(() => console.log('Got a response from the API'));
+  return promise
+    .then((res = { status: 200, body: 'success' }) => res)
+    .catch((error = new Error()) => error)
+    .finally(console.log('Got a response from the API'));
 }
