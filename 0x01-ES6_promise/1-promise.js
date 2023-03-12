e89c212da60645c0892a9db39e390cc03caab5ea
ngugimuchangi <tech.curious14@gmail.com>
2023-03-12 08:38:00 +0300

    debugging

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,4 +1,9 @@
 export default function getFullResponseFromAPI(success) {
-  if (success) return Promise.resolve({ status: 200, body: 'Success' });
-  return Promise.reject(new Error('The fake API is not working properly'));
+  return new Promise((resolve, reject) => {
+    if (success) {
+      resolve({ status: 200, body: 'Success' });
+    } else {
+      reject(Error('The fake API is not working properly'));
+    }
+  });
 }
