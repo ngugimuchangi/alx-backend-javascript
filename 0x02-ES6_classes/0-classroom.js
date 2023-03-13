d18c6fd9ea5550e10b19ff03351ba31345de83f4
ngugimuchangi <tech.curious14@gmail.com>
2023-03-13 18:39:46 +0300

    simple class

---
+++ 0x02-ES6_classes/0-classroom.js
@@ -0,0 +1,10 @@
+export default class classRoom {
+  /**
+   * class constructor
+   * @param {Number} maxStudentsSize
+   */
+  constructor(maxStudentsSize) {
+    // eslint-disable-next-line no-underscore-dangle
+    this._maxStudentsSize = maxStudentsSize;
+  }
+}
