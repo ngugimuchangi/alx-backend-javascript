50396a5e7763de489298674ce2bfca40e967e115
ngugimuchangi <tech.curious14@gmail.com>
2023-03-18 17:37:50 +0300

    added check fro empty set elements

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -5,12 +5,12 @@
  * @returns {String}
  */
 export default function cleanSet(set, startString) {
-  if (startString === '' || typeof startString !== 'string') return startString;
+  if (startString === '') return startString;
   const stringsArray = [];
-  for (const element of set.values()) {
-    if (element.slice(0, startString.length) === startString) {
+  set.forEach((element) => {
+    if (typeof element === 'string' && element.startsWith(startString)) {
       stringsArray.push(element.slice(startString.length));
     }
-  }
+  });
   return stringsArray.join('-');
 }
