d6052ff055e9fb94b2aa35380babf75a6c483187
ngugimuchangi <tech.curious14@gmail.com>
2023-03-18 17:52:26 +0300

    fixed prefix validation

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,11 +1,12 @@
 /**
- *
+ * Checks for a strings that start with given prefix and returns
+ * them as a one string separated by a dash
  * @param   {Set}     set
  * @param   {String}  startString
  * @returns {String}
  */
 export default function cleanSet(set, startString) {
-  if (typeof startString !== 'string' && startString === '') return '';
+  if (typeof startString !== 'string' || startString === '') return '';
   const stringsArray = [];
   set.forEach((element) => {
     if (typeof element === 'string' && element.startsWith(startString)) {
