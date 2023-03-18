ff28b168514ed60f3ffac8cb1792aa9435b085c7
ngugimuchangi <tech.curious14@gmail.com>
2023-03-18 17:53:07 +0300

    creating maps

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,13 @@
+/**
+ * Creates a map of grocery items and their quantities
+ * @returns {Map} map object created
+ */
+export default function groceriesList() {
+  const map = new Map();
+  map.set('Apple', 10);
+  map.set('Tomatoes', 10);
+  map.set('Pasta', 1);
+  map.set('Rice', 1);
+  map.set('Banana', 5);
+  return map;
+}
