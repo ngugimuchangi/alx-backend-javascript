4a2c69da998859cbd6920703ec04da8d3e270239
ngugimuchangi <tech.curious14@gmail.com>
2023-03-18 18:11:01 +0300

    docs update

--- 0x03-ES6_data_manipulation/9-groceries_list.js
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -4,7 +4,7 @@
  */
 export default function groceriesList() {
   const map = new Map();
-  map.set('Apple', 10);
+  map.set('Apples', 10);
   map.set('Tomatoes', 10);
   map.set('Pasta', 1);
   map.set('Rice', 1);
