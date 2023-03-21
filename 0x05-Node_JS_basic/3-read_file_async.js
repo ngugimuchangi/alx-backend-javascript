f055623e308aeabe2285f4f97e44f57c0fb6a53d
ngugimuchangi <tech.curious14@gmail.com>
2023-03-21 07:08:49 +0300

    added promise rejection

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -6,32 +6,34 @@ const fs = require('fs');
  * @param {string} dbPath - path to csv file.
  */
 function countStudents(dbPath) {
-  return new Promise((resolve) => {
+  return new Promise((resolve, reject) => {
     fs.readFile(dbPath, 'utf-8', (error, data) => {
-      if (error) throw new Error('Cannot load the database');
-      let students = data.split('\n');
-      students = students.slice(1, students.length - 1);
-      const courses = new Map();
+      if (!error) {
+        let students = data.split('\n');
+        students = students.slice(1, students.length - 1);
+        const courses = new Map();
 
-      // Parse CSV data creating a map of courseData objects.
-      students.forEach((student) => {
-        const studentData = student.split(',');
-        const firstName = studentData[0];
-        const field = studentData[3];
-        if (courses.has(field)) {
-          courses.get(field).students.push(firstName);
-          courses.get(field).count += 1;
-        } else {
-          courses.set(field, { students: [firstName], count: 1 });
-        }
-      });
+        // Parse CSV data creating a map of courseData objects.
+        students.forEach((student) => {
+          const studentData = student.split(',');
+          const firstName = studentData[0];
+          const field = studentData[3];
+          if (courses.has(field)) {
+            courses.get(field).students.push(firstName);
+            courses.get(field).count += 1;
+          } else {
+            courses.set(field, { students: [firstName], count: 1 });
+          }
+        });
 
-      // Display information from map
-      console.log(`Number of students: ${students.length}`);
-      courses.forEach((courseData, course) => {
-        console.log(`Number of students in ${course}: ${courseData.count}. List: ${courseData.students.join(', ')}`);
-      });
-      resolve();
+        // Display information from map
+        console.log(`Number of students: ${students.length}`);
+        courses.forEach((courseData, course) => {
+          console.log(`Number of students in ${course}: ${courseData.count}. List: ${courseData.students.join(', ')}`);
+        });
+        resolve();
+      }
+      reject(Error('Cannot load the database'));
     });
   });
 }
