b468817e0092fb43f99f16989306c33ac005e6b1
ngugimuchangi <tech.curious14@gmail.com>
2023-03-21 21:26:04 +0300

    added error message to response body incase of no db param

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -17,37 +17,35 @@ const app = http.createServer((req, resp) => {
   }
   if (req.url === '/students') {
     const body = ['This is the list of our students'];
-    if (!db) resp.end(body[0]);
-    else {
-      fs.readFile(db, 'utf-8', (error, data) => {
-        if (error) {
-          resp.end(body[0]);
-        } else {
-          const courses = new Map();
-          let students = data.split('\n');
-          students = students.slice(1, students.length - 1);
-
-          // Parse CSV data creating a map of courseData objects.
-          students.forEach((student) => {
-            const studentData = student.split(',');
-            const firstName = studentData[0];
-            const field = studentData[3];
-            if (courses.has(field)) {
-              courses.get(field).push(firstName);
-            } else {
-              courses.set(field, [firstName]);
-            }
-          });
-
-          // Organize data in an array
-          body.push(`Number of students: ${students.length}`);
-          courses.forEach((courseStudents, course) => {
-            body.push(`Number of students in ${course}: ${courseStudents.length}. List: ${courseStudents.join(', ')}`);
-          });
-        }
+    fs.readFile(db, 'utf-8', (error, data) => {
+      if (error) {
+        body.push('Cannot load the database');
         resp.end(body.join('\n'));
-      });
-    }
+      } else {
+        const courses = new Map();
+        let students = data.split('\n');
+        students = students.slice(1, students.length - 1);
+
+        // Parse CSV data creating a map of courseData objects.
+        students.forEach((student) => {
+          const studentData = student.split(',');
+          const firstName = studentData[0];
+          const field = studentData[3];
+          if (courses.has(field)) {
+            courses.get(field).push(firstName);
+          } else {
+            courses.set(field, [firstName]);
+          }
+        });
+
+        // Organize data in an array
+        body.push(`Number of students: ${students.length}`);
+        courses.forEach((courseStudents, course) => {
+          body.push(`Number of students in ${course}: ${courseStudents.length}. List: ${courseStudents.join(', ')}`);
+        });
+      }
+      resp.end(body.join('\n'));
+    });
   }
 });
 
