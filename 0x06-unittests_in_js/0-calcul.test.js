fed433bf8b1d0c5da821ab2ace1c921b9d438b1e
ngugimuchangi <tech.curious14@gmail.com>
2023-03-23 16:00:22 +0300

    more testcase scenarios

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -7,6 +7,7 @@ const calculateNumber = require('./0-calcul');
 describe('calculateNumber simple test suite', function() {
   it('should round its arguments and return their sum', function() {
     assert.equal(calculateNumber(1, 3), 4);
+    assert.equal(calculateNumber(1, 3.7), 5);
     assert.equal(calculateNumber(1.2, 3.7), 5);
     assert.equal(calculateNumber(1.5, 3.7), 6);
   });
