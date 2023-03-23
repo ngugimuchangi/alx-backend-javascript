3be0d2d2e019c4b58ad96215db6cde1e14c5ed3f
ngugimuchangi <tech.curious14@gmail.com>
2023-03-23 09:34:51 +0300

    test suite structure update

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -7,11 +7,13 @@ const Utils = require('./utils');
 const sendPaymentRequestToAPi = require('./3-payment');
 
 describe('sendPaymentRequestToApi', function() {
-  it('should ensure Utils.calculate is called and with the right args', function() {
-    const spy = sinon.spy(Utils, 'calculateNumber');
-    sendPaymentRequestToAPi(100, 20);
-    expect(spy.calledOnce).to.equal(true);
-    expect(spy.calledWith('SUM', 100, 20)).to.equal(true);
-    spy.restore();
+  describe('#Utils.calculateNumber()', function() {
+    it('should be called with args from sendPaymentRequestToApi', function() {
+      const spy = sinon.spy(Utils, 'calculateNumber');
+      sendPaymentRequestToAPi(100, 20);
+      expect(spy.calledOnce).to.equal(true);
+      expect(spy.calledWith('SUM', 100, 20)).to.equal(true);
+      spy.restore();
+    });
   });
 });
--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -7,15 +7,17 @@ const Utils = require('./utils');
 const sendPaymentRequestToAPi = require('./4-payment');
 
 describe('sendPaymentRequestToApi', function() {
-  it('should ensure Utils.calculate is called and with the right args', function() {
-    const stub = sinon.stub(Utils, 'calculateNumber');
-    const spy = sinon.spy(console, 'log');
-    stub.withArgs('SUM', 100, 20).returns(10);
-    sendPaymentRequestToAPi(100, 20);
-    expect(stub.calledOnce).to.equal(true);
-    expect(stub.calledWith('SUM', 100, 20)).to.equal(true);
-    expect(spy.calledWith('The total is: 10')).to.equal(true);
-    stub.restore();
-    spy.restore();
+  describe('#stubbed Utils.calculateNumber()', function() {
+    it('should be return 10 when called by sendPaymentRequestToAPI', function() {
+      const stub = sinon.stub(Utils, 'calculateNumber');
+      const spy = sinon.spy(console, 'log');
+      stub.withArgs('SUM', 100, 20).returns(10);
+      sendPaymentRequestToAPi(100, 20);
+      expect(stub.calledOnce).to.equal(true);
+      expect(stub.calledWith('SUM', 100, 20)).to.equal(true);
+      expect(spy.calledWith('The total is: 10')).to.equal(true);
+      stub.restore();
+      spy.restore();
+    });
   });
 });
