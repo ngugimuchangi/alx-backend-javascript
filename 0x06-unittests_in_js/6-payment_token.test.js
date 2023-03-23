f79e9516d7b95c2d7bd93cf88cee70007814971b
ngugimuchangi <tech.curious14@gmail.com>
2023-03-23 13:06:50 +0300

    fixed callback call point

--- 0x06-unittests_in_js/6-payment_token.test.js
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -6,14 +6,15 @@ const getPaymentTokenFromAPI = require('./6-payment_token');
 
 describe('getPaymentTokenFromAPI', function() {
   describe('#Async test', function() {
-    it('should get test response from payment API', function(done) {
-      expect(getPaymentTokenFromAPI(false)).to.equal(undefined);
+    
+    it('should test response from payment API', function(done) {
+      // expect(getPaymentTokenFromAPI(false)).to.equal(undefined);
       getPaymentTokenFromAPI(true)
         .then((res) => {
           expect(res).to.be.an('object')
             .with.property('data', 'Successful response from the API');
+          done();
         });
-      done();
     });
   });
 });
