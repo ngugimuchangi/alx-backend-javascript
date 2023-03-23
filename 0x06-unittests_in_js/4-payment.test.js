a0b8499026c60ea6f9089d64b289a084658049d0
ngugimuchangi <tech.curious14@gmail.com>
2023-03-23 09:20:12 +0300

    fix: test script and stub args

--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -10,7 +10,7 @@ describe('sendPaymentRequestToApi', function() {
   it('should ensure Utils.calculate is called and with the right args', function() {
     const stub = sinon.stub(Utils, 'calculateNumber');
     const spy = sinon.spy(console, 'log');
-    stub.withArgs('SUM').returns(10);
+    stub.withArgs('SUM', 100, 20).returns(10);
     sendPaymentRequestToAPi(100, 20);
     expect(stub.calledOnce).to.equal(true);
     expect(stub.calledWith('SUM', 100, 20)).to.equal(true);
