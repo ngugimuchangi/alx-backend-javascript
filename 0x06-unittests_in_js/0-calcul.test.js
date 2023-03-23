87af4b8799da920714c0161a3277c74424179635
ngugimuchangi <tech.curious14@gmail.com>
2023-03-23 16:12:50 +0300

    tests for negatives

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -6,11 +6,17 @@ const calculateNumber = require('./0-calcul');
 
 describe('calculateNumber simple test suite', function() {
   describe('#Add two numbers', function() {
-    it('should round its arguments and return their sum', function() {
+    it('should round positive numbers', function() {
       assert.equal(calculateNumber(1, 3), 4);
       assert.equal(calculateNumber(1, 3.7), 5);
       assert.equal(calculateNumber(1.2, 3.7), 5);
       assert.equal(calculateNumber(1.5, 3.7), 6);
+    });
+    it('should round round negative numbers', function() {
+      assert.equal(calculateNumber(-1, 3), 2);
+      assert.equal(calculateNumber(-1.6, 3), 1);
+      assert.equal(calculateNumber(-1.6, -3), -5);
+      assert.equal(calculateNumber(-1.6, -3.6), -6);
     });
   });
 });
