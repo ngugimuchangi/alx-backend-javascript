69009cc8997c50bef15c1536661b161cf1a8f006
ngugimuchangi <tech.curious14@gmail.com>
2023-03-23 16:03:54 +0300

    updated test structure

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -5,10 +5,12 @@ const assert = require('assert');
 const calculateNumber = require('./0-calcul');
 
 describe('calculateNumber simple test suite', function() {
-  it('should round its arguments and return their sum', function() {
-    assert.equal(calculateNumber(1, 3), 4);
-    assert.equal(calculateNumber(1, 3.7), 5);
-    assert.equal(calculateNumber(1.2, 3.7), 5);
-    assert.equal(calculateNumber(1.5, 3.7), 6);
+  describe('#Add two numbers', function() {
+    it('should round its arguments and return their sum', function() {
+      assert.equal(calculateNumber(1, 3), 4);
+      assert.equal(calculateNumber(1, 3.7), 5);
+      assert.equal(calculateNumber(1.2, 3.7), 5);
+      assert.equal(calculateNumber(1.5, 3.7), 6);
+    });
   });
 });
