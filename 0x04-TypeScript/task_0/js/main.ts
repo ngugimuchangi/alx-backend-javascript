c97e15df0758242ee0332c5830a8ec27cfd2a52b
ngugimuchangi <tech.curious14@gmail.com>
2023-03-24 07:11:25 +0300

    task 1

---
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -0,0 +1,44 @@
+interface Student {
+    firstName: string;
+    lastName: string;
+    age: number;
+    location: string;
+}
+
+const studentOne: Student = {
+    firstName: 'John',
+    lastName: 'Doe',
+    age: 20,
+    location: 'California'
+}
+const studentTwo: Student = {
+    firstName: 'Jane',
+    lastName: 'Doe',
+    age: 21,
+    location: 'Silicon Valley'
+}
+const students: Student[] = [studentOne, studentTwo];
+const table = document.createElement('table');
+table.style.border = '1px solid black'
+
+// Add headers
+const tableHeader = document.createElement('tr');
+const firstNameHeader = document.createElement('th');
+const locationHeader = document.createElement('th');
+firstNameHeader.innerText = 'Name';
+locationHeader.innerText = 'Location';
+tableHeader.append(firstNameHeader, locationHeader);
+table.appendChild(tableHeader);
+
+students.forEach((student) => {
+    // Add table data
+    const row = document.createElement('tr');
+    const firstName = document.createElement('td');
+    const location = document.createElement('td');
+    firstName.innerText = student.firstName;
+    location.innerText = student.location;
+    row.append(firstName, location);
+    table.append(row);
+});
+
+document.body.appendChild(table);
