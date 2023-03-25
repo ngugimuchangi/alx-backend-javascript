c7dc55195a5cdd25abf9d6c8ac2ae47472c23e8a
ngugimuchangi <tech.curious14@gmail.com>
2023-03-25 15:03:11 +0300

    refactored tested for clarity

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -5,18 +5,34 @@ const assert = require('assert');
 const calculateNumber = require('./0-calcul');
 
 describe('calculateNumber simple test suite', function() {
-  describe('#Add two numbers', function() {
-    it('should round positive numbers', function() {
+  describe('#Add positive numbers', function() {
+    it('should return 4', function() {
       assert.equal(calculateNumber(1, 3), 4);
+    });
+    it('should return 5', function() {
       assert.equal(calculateNumber(1, 3.7), 5);
+    });
+    it('should return 5', function() {
       assert.equal(calculateNumber(1.2, 3.7), 5);
+    });
+    it('should return 6', function() {
       assert.equal(calculateNumber(1.5, 3.7), 6);
     });
-    it('should round round negative numbers', function() {
-      assert.equal(calculateNumber(-1, 3), 2);
-      assert.equal(calculateNumber(-1.6, 3), 1);
+  });
+  describe('#Add negative numbers', function() {
+    it('should return -5', function() {
       assert.equal(calculateNumber(-1.6, -3), -5);
+    });
+    it('should return -6', function() {
       assert.equal(calculateNumber(-1.6, -3.6), -6);
+    });
+  });
+  describe('#Add positive and negative numbers', function() {
+    it('should return 2', function() {
+      assert.equal(calculateNumber(-1, 3), 2);
+    });
+    it('should return 1', function() {
+      assert.equal(calculateNumber(-1.6, 3), 1);
     });
   });
 });
